953b2a44a1d0c530e87ca50d790c1b21bf6fb644
Pera Urosevic <pera.urosevic@gmail.com>
2025-01-09 09:31:59 +0200

    cleaner: replace recycle bin cmd

--- api/cleaner/filesystem/delete.go
+++ api/cleaner/filesystem/delete.go
@@ -12,8 +12,8 @@ func Delete(record types.RecordCleaner) error {
 	root := os.Getenv("CLEANER_ROOT")
 	path := root + record.Path + "/" + record.Name
 	path = strings.ReplaceAll(path, "/", "\\")
-	cmd := "pwsh.exe"
-	args := []string{"-c", "recycle.ps1", path}
+	cmd := "recycle-bin.exe"
+	args := []string{path}
 	log.Log("[DELETE]", path)
 	run := exec.Command(cmd, args...)
 	_, err := run.CombinedOutput()
